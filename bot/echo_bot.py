6c7cbf823194bc52210e9c0180cbb8c2384623d0
roddy-reyess <sergireyes26@gmail.com>
2019-10-02 16:39:46 +0200

    prueba 2

--- bot/echo_bot.py
+++ bot/echo_bot.py
@@ -6,3 +6,9 @@ bot = telebot.TeleBot("824465608:AAG1U3q3CzxLX0aYHNfX4Eyk4-Eldv-XK9Q")
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Hola, qué tal estás?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
