594de23224e776b86be0e6c1616d1ac27a3372bb
roddy-reyess <sergireyes26@gmail.com>
2019-10-02 18:06:03 +0200

    affsadfdf

--- bot/echo_bot.py
+++ bot/echo_bot.py
@@ -5,7 +5,16 @@ bot = telebot.TeleBot("824465608:AAG1U3q3CzxLX0aYHNfX4Eyk4-Eldv-XK9Q")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Hola, qué tal estás?")
+    print(message.chat.id)
+    if message.text == "/start":
+        	bot.send_message(message.chat.id, """Bienvenido a roddybot!! \n \n Un bot hecho para divertirte y pasar un buen rato.""")
+    elif message.text == "/help":
+        bot.send_message(message.chat.id, """Lista de comandos!! \n\n /start - Te permite iniciar el bot \n /help muestra los comandos posibles \n /character""")
+
+@bot.message_handler(commands=['character'])
+def create_character(message):
+    personaje = []
+    bot.send_message(message.chat.id, "Quieres crear un personaje?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
